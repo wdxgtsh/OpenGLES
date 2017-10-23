5a1206e275dd0e976fbbb09a79a442457b55a9e7
zhaolei <zhaolei@allinmd.cn>
2017-10-23 17:52:29 +0800

    init project

---
+++ 001-OpenGL ES/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  001-OpenGL ES
+//
+//  Created by zhaolei on 2017/10/23.
+//  Copyright © 2017年 zhaolei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 001-OpenGL ES/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  001-OpenGL ES
+//
+//  Created by zhaolei on 2017/10/23.
+//  Copyright © 2017年 zhaolei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
